9080ed61dd61daa89c5f331bac3d0cdfb153234c
Tim Kuminecz <tkuminecz@gmail.com>
2017-01-23 03:39:40 -0500

    cleanup; add eslint

--- src/main.js
+++ src/main.js
@@ -1,68 +1,11 @@
 // @flow
-import type { $TList, $1Type, $T1, $T2 } from 'flow-higher';
-import { HKT, t1 } from 'flow-higher';
+import type { $TList } from 'flow-higher';
 
-/**
- * DataType class
- */
-export class DataType<Kind, TypeSignature: $TList<any, any>, Instance, Data> {
+export class TypeDef<Sig: $TList<any, any>, Opts: { data: any }> {} // eslint-disable-line no-unused-vars
 
-	instance: Instance
+export class ADT<K, T> { // eslint-disable-line no-unused-vars
 
-	data: Data
-
-	constructor(kind: Class<Kind>, instance: Instance, data: Data): void {
-		this.instance = instance;
-		this.data = data;
-	}
-
-	/**
-	 * wrap :: Class Kind -> Data -> DataType Kind TypeSignature Data
-	 */
-	static wrap<Kind, TSig: $TList<any, any>, Instance, Data, DataTypeClass: DataType<Kind, TSig, Instance, Data>>(dataType: DataTypeClass, data: Data): HKT<DataTypeClass, TypeSignature> {
-		return ((data: any): HKT<DataTypeClass, TypeSignature>);
-	}
-
-	/**
-	 * unwrap :: Class Kind -> DataType Kind TypeSignature Data -> Data
-	 */
-	static unwrap<Kind, TypeSignature: $TList<any, any>, Data>(kind: Class<Kind>, wrappedData: DataType<Kind, TypeSignature, Data>): Data {
-		return ((wrappedData: any): Data);
-	}
-
-}
-
-
-class IsMaybe {}
-
-type MaybeData<T> =
-	| { tag: 'Just', value: $T1<T> }
-	| { tag: 'Nothing' }
-
-// type $Maybe<T> = KindWithInstance<IsMaybe, Maybe, MaybeData<T>>
-
-// class MaybeType<T: $TList<any, any>> extends DataType<IsMaybe, T, MaybeData<T>> {}
-
-// class Maybe {}
-
-
-
-class IsEither {}
-
-type Either<T> =
-	| { tag: 'Left', value: $T1<T> }
-	| { tag: 'Right', value: $T2<T> }
-
-
-
-
-class TypeDef<Sig, Opts: { data: any }> {
-
-}
-
-class ADT<K, T> {
-
-	static wrap<Sig, Data, Def: TypeDef<Sig, { data: Data }>>(k: Class<Def>, data: Data): ADT<Def, Sig> {
+	static wrap<Sig: $TList<any, any>, Data, Def: TypeDef<Sig, { data: Data }>>(k: Class<Def>, data: Data): ADT<Def, Sig> {
 		return ((data: any): ADT<Def, Sig>);
 	}
 
@@ -72,50 +15,10 @@ class ADT<K, T> {
 
 }
 
-type TypeSpec<T, I> = {
-	type: T,
-	instance: I
-}
-
-class Instance<K> {
-	Kind: Class<K>
-	constructor(kind: Class<K>): void {
-		this.Kind = kind;
-	}
-}
-
-function createDataType<Kind, Sig, TypeInstance: *, Data, ThisAdt: TypeDef<Sig, { data: Data }>>(
+export function createDataType<Sig: $TList<any, any>, TypeInstance: *, Data, ThisAdt: TypeDef<Sig, { data: Data }>>(
 	TypeSpec: Class<ThisAdt>,
 	instanceClassFn: (type: Class<ThisAdt>) => Class<TypeInstance>
 
 ): TypeInstance  {
-	const KindClass = ((class {}: any): Class<Kind>);
-
-	const InstanceClass = instanceClassFn(TypeSpec);
-
-	return new InstanceClass();
+	return new instanceClassFn(TypeSpec)();
 }
-
-
-const Maybe2 = createDataType(
-	class MaybeData<T: $1Type<any>> extends TypeDef<T, {
-		data:
-			| { tag: 'Just', value: $T1<T> }
-			| { tag: 'Nothing' }
-	}> {},
-	(T) => class Maybe {
-
-		Kind = T
-
-		Just<A: *>(a: A): ADT<T, $1Type<A>> {
-			return ADT.wrap(T, { tag: 'Just', value: a });
-		}
-
-		Nothing(): ADT<T, $1Type<*>> {
-			return ADT.wrap(T, { tag: 'Nothing' });
-		}
-
-	}
-);
-
-(Maybe2.Just(42): ADT<Maybe2.Kind, $1Type<number>>);
--- test/main.js
+++ test/main.js
@@ -1,8 +1,39 @@
 // @flow
-import {} from '../src/main';
+import type { $1Type, $T1 } from 'flow-higher';
+import { ADT, createDataType, TypeDef } from '../src/main';
 import test from 'tape';
 
 test('flow-adt', t => {
 	t.plan(1);
 	t.pass('stub');
 });
+
+/**
+ * flow tests
+ */
+if (false) { // eslint-disable-line no-constant-condition
+
+	const Maybe2 = createDataType(
+		class MaybeData<T: $1Type<any>> extends TypeDef<T, {
+			data:
+				| { tag: 'Just', value: $T1<T> }
+				| { tag: 'Nothing' }
+		}> {},
+		(T) => class Maybe {
+
+			Kind = T
+
+			Just<A: *>(a: A): ADT<T, $1Type<A>> {
+				return ADT.wrap(T, { tag: 'Just', value: a });
+			}
+
+			Nothing(): ADT<T, $1Type<*>> {
+				return ADT.wrap(T, { tag: 'Nothing' });
+			}
+
+		}
+	);
+
+	(Maybe2.Just(42): ADT<Maybe2.Kind, $1Type<number>>); // eslint-disable-line no-unused-expressions
+
+}
