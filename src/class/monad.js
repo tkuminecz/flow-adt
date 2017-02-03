f0650be4a540795bfc0c4a884ce7b29f742976cf
Tim Kuminecz <tkuminecz@gmail.com>
2017-02-03 01:05:53 -0500

    add andThen() method to Monad

--- src/class/monad.js
+++ src/class/monad.js
@@ -1,8 +1,11 @@
 // @flow
+import type { $A, $App, $SwapA } from 'flow-higher';
 import type { Applicative } from './applicative';
 import type { Chain } from './chain';
 
 /**
  * `Monad` type class
  */
-export interface Monad<K, T> extends Applicative<K, T>, Chain<K, T> {}
+export interface Monad<K, T> extends Applicative<K, T>, Chain<K, T> {
+	andThen<B>(next: (a: $A<T>) => $App<K, $SwapA<T, B>>, ma: $App<K, T>): $App<K, $SwapA<T, B>>
+}
